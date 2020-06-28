42feca155a9bfe18a1ced7208507ac60072040fa
zhangfu <zhangfu@nasetech.com>
2020-06-28 09:17:52 +0800

    fix: 升级qiankun解决子应用刷新问题

--- microMaster/src/app.js
+++ microMaster/src/app.js
@@ -26,5 +26,20 @@ setTimeout(() => {
         }
       }
     ],
+    {
+      beforeLoad: (app) => {
+        // 加载微应用前，加载进度条
+     
+        console.log("before load", app.name);
+        return Promise.resolve();
+      },
+      // qiankun 生命周期钩子 - 微应用挂载后
+      afterMount: (app) => {
+        // 加载微应用前，进度条加载完成
+     
+        console.log("after mount", app.name);
+        return Promise.resolve();
+      },
+    }
   )
 })
